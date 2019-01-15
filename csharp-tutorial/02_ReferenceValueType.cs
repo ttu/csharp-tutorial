196d8f303baa16f25cf4be0dc0f22b5397d339b3
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2019-01-15 23:11:15 +0200

    Add collecion out example

--- csharp-tutorial/02_ReferenceValueType.cs
+++ csharp-tutorial/02_ReferenceValueType.cs
@@ -1,15 +1,26 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace csharp_tutorial
 {
     public class RefVal
     {
+        /*
+         * The Types in .NET Framework are either treated by Value Type or by Reference Type.
+         * A Value Type holds the data within its own memory allocation and
+         * a Reference Type contains a pointer to another memory location that holds the real data.
+         * Reference Type variables are stored in the heap while Value Type variables are stored in the stack.
+        */
+
         // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/passing-parameters
         // Types are passed to methods by value
         // Passing a value-type variable to a method by value means passing a copy of the variable to the method
         // A variable of a reference type does not contain its data directly; it contains a reference to its data
 
         // http://net-informations.com/faq/general/valuetype-referencetype.htm
+
+        // Coffee cup example
+        // https://www.mathwarehouse.com/programming/passing-by-value-vs-by-reference-visual-explanation.php
 
         [Fact]
         public void Update_Value()
@@ -81,16 +92,34 @@ namespace csharp_tutorial
             Assert.Equal(5, myValue);
         }
 
+        [Fact]
+        public void Out_Collection()
+        {
+            var dict = new Dictionary<string, Person>
+            {
+                ["111"] = new Person { Name = "Roger" }
+            };
+
+            if (dict.TryGetValue("111", out var person))
+            {
+                person.Name = "Steve";
+            }
+        }
+
         private class User
         {
             public string Ssn { get; set; }
 
             public string Name { get; set; }
 
-            public override bool Equals(object obj) => obj is User ? Ssn == ((User)obj).Ssn : false;
+            public override bool Equals(object obj) => obj is User us ? Ssn == us.Ssn : false;
 
+            // Some collections group items to buckets by hashcode
             public override int GetHashCode() => Ssn.GetHashCode();
         }
+
+        // For predefined value types, the equality operator (==) returns true if the values of its operands are equal, false otherwise.
+        // For reference types other than string, == returns true if its two operands refer to the same object.
 
         [Fact]
         public void EqualsExample()
