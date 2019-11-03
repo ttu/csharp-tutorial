4a6689aa33b2053cea6067d615b9feee2d652e3b
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2019-11-03 16:28:53 +0200

    Add nullable reference type example

--- csharp-tutorial/03_Null.cs
+++ csharp-tutorial/03_Null.cs
@@ -1,4 +1,7 @@
+using Newtonsoft.Json;
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace csharp_tutorial
@@ -26,6 +29,15 @@ namespace csharp_tutorial
             string print3 = value ?? "default value";
 
             Assert.Equal("This has something", print3);
+        }
+
+        [Fact]
+        public void Null_Coalescing_CompoundAssignment()
+        {
+            string value = string.Empty;
+
+            //value = value ?? "default value";
+            value ??= "default value";
         }
 
         public class NullExample
@@ -87,5 +99,88 @@ namespace csharp_tutorial
                 // Do something
             }
         }
+
+#nullable enable
+// Normally would just add <Nullable>enabled</Nullable> to csproj-file to enable nullable for the whole project
+
+        [Fact]
+        public void Nullable_ReferenceTypes()
+        {
+            var sensor = GetSensor(0);
+            var data = GetData(sensor);
+            Assert.Equal(3, data);
+        }
+
+        public SensorDto GetSensor(int id)
+        {
+            if (id == 0)
+            {
+                return new SensorDto { Data = 3 };
+            }
+            else
+            {
+                throw new Exception("Sensor not found");
+                // if return value would be nullable (SensorDto?), then null would be possible 
+                //return null;
+            }
+        }
+
+        public double GetData(SensorDto sensor)
+        {
+            // No need for null check as sensor can't be null
+            return sensor.Data;
+        }
+
+        [Fact]
+        public void Nullable_CanBeNull()
+        {
+            var sensor = GetSensorCanBeNull(0);
+
+            if (sensor == null)
+                return;
+
+            var data = GetData(sensor);
+            Assert.Equal(3, data);
+        }
+
+        public SensorDto? GetSensorCanBeNull(int id)
+        {
+            if (id == 0)
+            {
+                return new SensorDto();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public double GetDataCanBeNull(SensorDto? sensor)
+        {
+            if (sensor == null)
+                return double.MinValue;
+
+            return sensor.Data;
+        }
+
+        // Get GetSensorAsync with nullable reference type
+        public static async Task<SensorDto?> GetSensorAsync(string sensrorId = "iddqd")
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://dummy-sensors.azurewebsites.net/api/sensor/{sensrorId}");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                    //throw new Exception("Sensor not found");
+                }
+
+                var sensorJson = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<SensorDto>(sensorJson);
+            }
+        }
+
+#nullable disable
     }
 }
